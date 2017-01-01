414eee293bce264dccae17ddeb0143a762e2e8e8
이경문 <leekm8474@gmail.com>
2017-01-01 15:33:57 +0900

    커밋

--- CanGo/CanGo/MainViewController.swift
+++ CanGo/CanGo/MainViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class MainViewController: UIViewController,MTMapViewDelegate, UISearchBarDelegate{
     
     lazy var mapView: MTMapView = MTMapView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+
+    var items:[MTMapPOIItem] = []
     
     var leftbutton:UIButton = UIButton(frame: CGRect(x: -3, y: 8, width: 66, height: 52))
     var searchbar:UISearchBar = UISearchBar(frame: CGRect(x: 63, y: 12, width: 309, height: 44))
@@ -18,18 +20,10 @@ class MainViewController: UIViewController,MTMapViewDelegate, UISearchBarDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         searchbar.delegate = self
-        mapView.addPOIItems([custom_item()])
+        //mapView.addPOIItems([custom_item()])
         leftbutton.setImage(UIImage(named: "app_002_3_1 copy"), for: .normal)
         searchbar.placeholder = "장소검색"
         searchbar.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
-        
-//        UITextField *txfSearchField = [searchbar valueForKey:@"_searchField"];
-//        [txfSearchField setBackgroundColor:[UIColor whiteColor]];
-//        [txfSearchField setLeftView:UITextFieldViewModeNever];
-//        [txfSearchField setBorderStyle:UITextBorderStyleRoundedRect];
-//        txfSearchField.layer.borderWidth = 8.0f;
-//        txfSearchField.layer.cornerRadius = 10.0f;
-//        txfSearchField.layer.borderColor = [UIColor clearColor].CGColor;
         
         
         
@@ -51,21 +45,7 @@ class MainViewController: UIViewController,MTMapViewDelegate, UISearchBarDelegat
         Server.manager.search(place: searchBar.text!, completion: {
             result in
             print(result)
-            func custom_item() -> MTMapPOIItem{
-                
-                
-                let poiItem = MTMapPOIItem()
-                poiItem.itemName = "이름"
-                poiItem.markerType = .customImage
-                poiItem.customImageName = "app6_7"
-                //poiItem.markerSelectedType = .customImage                   //선택 되었을 때 마커 타입
-                //poiItem.customSelectedImage = UIImage(named: "app6_6")    //선택 되었을 때 마커 이미지 지정
-                poiItem.mapPoint = MTMapPoint(geoCoord: .init(latitude: 37.5654338, longitude: 126.976664))
-                poiItem.showAnimationType = .noAnimation
-                poiItem.customImageAnchorPointOffset = .init(offsetX: 0, offsetY: 0)
-                
-                return poiItem
-            }
+            
             
             let item = MTMapPOIItem()
             item.itemName = "asdf"
@@ -74,16 +54,33 @@ class MainViewController: UIViewController,MTMapViewDelegate, UISearchBarDelegat
             item.mapPoint = MTMapPoint(geoCoord: .init(latitude: 37.5654338, longitude: 126.976664))
             item.showAnimationType = .noAnimation
             item.customImageAnchorPointOffset = .init(offsetX: 30, offsetY: 0)    // 마커 위치 조정
-            
-            
-            
+
+            //items.append(item)
+            self.mapView.add(item)
         })
     }
-    
-    
+    /*
+     func custom_item() -> MTMapPOIItem{
+
+
+     let poiItem = MTMapPOIItem()
+     poiItem.itemName = "이름"
+     poiItem.markerType = .customImage
+     poiItem.customImageName = "app6_7"
+     //poiItem.markerSelectedType = .customImage                   //선택 되었을 때 마커 타입
+     //poiItem.customSelectedImage = UIImage(named: "app6_6")    //선택 되었을 때 마커 이미지 지정
+     poiItem.mapPoint = MTMapPoint(geoCoord: .init(latitude: 37.5654338, longitude: 126.976664))
+     poiItem.showAnimationType = .noAnimation
+     poiItem.customImageAnchorPointOffset = .init(offsetX: 0, offsetY: 0)
+
+     return poiItem
+     }
+     */
+
+
     /*
      // MARK: - Navigation
-     
+
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destinationViewController.
